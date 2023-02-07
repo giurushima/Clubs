9ed33c0908f140f55288f551e95d8ca7631d9d18
giurushima <giurushima@hotmail.com>
2023-02-07 19:59:12 -0300

    DTOs de jugador creados

---
+++ Models/PlayerToUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using Clubs.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Clubs.Models
+{
+    public class PlayerToUpdateDto
+    {
+        [Required(ErrorMessage = "Agrega un nombre completo")]
+        [MaxLength(50)]
+        public string FullName { get; set; } = string.Empty;
+        [MaxLength(50)]
+        public string Nationality { get; set; }
+        [MaxLength(50)]
+        public Position Position { get; set; }
+    }
+}
