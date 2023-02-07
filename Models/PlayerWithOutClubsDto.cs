6b0453261976108f48cf3f5516e8fa631d08b543
giurushima <giurushima@hotmail.com>
2023-02-07 20:01:17 -0300

    DTO de jugador para club

---
+++ Models/PlayerWithOutClubsDto.cs
@@ -0,0 +1,11 @@
+﻿using Clubs.Enums;
+
+namespace Clubs.Models
+{
+    public class PlayerWithOutClubsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public League League { get; set; }
+    }
+}
