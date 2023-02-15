4e504918e0e26594b364085f4bb3e5dc48d5924f
“ddewde“ <martinaguirre80@gmail.com>
2023-02-15 19:28:23 -0300

    ClubController - Get, GetClub, POST

--- Controllers/ClubsController.cs
+++ Controllers/ClubsController.cs
@@ -3,6 +3,7 @@ using Clubs.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Clubs.Models;
+using Clubs.Entities;
 
 namespace Clubs.Controllers
 {
@@ -17,6 +18,46 @@ namespace Clubs.Controllers
         {
             _infoClubsRepository = IInfoClubsRepository;
             _mapper = mapper;
+        }
+        [HttpGet]
+        //el famoso GetAll
+        public JsonResult GetClubs()
+        {
+            
+            return new JsonResult(_infoClubsRepository.GetClubs());
+
+            //buscar Clubes con la instancia y pasarlo como parámetro (esto se hacía antes de la inyecíón de dependencia)
+
+        }
+        [HttpGet("{idClub}")]
+        public IActionResult GetClub(int idClub)
+        {
+            Entities.Club? club = _infoClubsRepository.GetClub(idClub);
+            if (club is null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(_mapper.Map<ClubDto>(club));   
+        }
+        [HttpPost(Name = "GetClubs")]
+        public ActionResult<ClubDto> AgregarClub(ClubToCreateDto ClubToCreate)
+        {
+            Club clubNew = _mapper.Map<Club>(ClubToCreate);
+
+            _infoClubsRepository.AgregarClub(clubNew);
+
+            _infoClubsRepository.GuardarCambios();
+
+
+            return CreatedAtRoute(
+                        "getclubs",
+                        new
+                        {
+                            idClub = clubNew.Id
+
+                        },
+                        _mapper.Map<ClubDto>(clubNew));
         }
 
     }
