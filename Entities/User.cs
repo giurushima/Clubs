873476eccb70b337075f028c6529daaca1f342dd
giurushima <giurushima@hotmail.com>
2023-03-06 20:47:03 -0300

    Agregada la entidad de Users

---
+++ Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using Clubs.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Clubs.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(32)]
+        public string? Name { get; set; }
+        [MaxLength(32)]
+        public string? LastName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        public UserTypes Role { get; set; }
+
+    }
+}
