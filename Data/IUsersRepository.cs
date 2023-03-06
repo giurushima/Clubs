6cf8a690325ae67c9960a875a8e93d9086643dd1
giurushima <giurushima@hotmail.com>
2023-03-06 20:46:12 -0300

    Agregado de carpeta Data para la autenticacion

---
+++ Data/IUsersRepository.cs
@@ -0,0 +1,13 @@
+﻿using Clubs.Entities;
+using Clubs.Models;
+using static Clubs.Controllers.AuthenticationController;
+
+namespace Clubs.Data
+{
+    public interface IUsersRepository
+    {
+        public User? ValidateUser(AuthenticationRequestBody authRequestBody);
+        public bool SaveChange();
+    }
+    
+}
---
+++ Data/UsersRepository.cs
@@ -0,0 +1,26 @@
+﻿using Clubs.DBContexts;
+using Clubs.Entities;
+using Clubs.Models;
+
+namespace Clubs.Data
+{
+    public class UsersRepository : IUsersRepository
+    {
+        internal readonly ClubsContext _context;
+
+        public UsersRepository(ClubsContext context)
+        {
+            this._context = context;
+        }
+
+        public User? ValidateUser(AuthenticationRequestBody authRequestBody)
+        {
+            return _context.Users.FirstOrDefault(p => p.UserName == authRequestBody.UserName && p.Password == authRequestBody.Password);
+        }
+
+        public bool SaveChange()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
